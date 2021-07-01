9a376362a968fa4c6347f28bb7f8b834b97a3d33
Diego <diegodsha@hotmail.com>
2021-07-01 14:57:39 -0500

    Add App.js

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+import {Display} from './Display'
+import {ButtonPanel} from './ButtonPanel'
+
+const App = props => {
+    return (
+        <Fragment>
+            <Display />
+            {/* <ButtonPanel /> */}
+        </Fragment>
+    )
+}
+
+// App.propTypes = {
+
+// }
+
+export default App
