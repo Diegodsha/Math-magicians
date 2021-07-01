6905e312851684ef42a7f62366a3d32e9c070ab2
Diego <diegodsha@hotmail.com>
2021-07-01 14:58:22 -0500

    Add button and button panel to display

---
+++ src/Components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = ({name}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Button.propTypes = {
+name: PropTypes.string
+}
+
+export default Button
