dcb00d1673ef9a8472ed8d55aaab5baa6e07d9e9
Diego <diegodsha@hotmail.com>
2021-07-02 16:32:02 -0500

    Add more cases to calculate file

--- src/Logic/Calculate.js
+++ src/Logic/Calculate.js
@@ -1,35 +1,63 @@
-const calculate = (calcDataObject, btnName) =>{
-    const {total, next, operation} = calcDataObject
+const calculate = (calcDataObject, btnName) => {
+  const { total, next, operation } = calcDataObject;
 
   switch (btnName) {
-      case 'AC':
-          return {
-              total:null,
-              next:null,
-              operation:null
-          }
-          case'+/-':
-          if (total){
-              return{
-                  total: `${total * - 1}`,
-                  next,
-                  operation:null
-              }
-          }
-          if(next){
-              return{
-                total: `${total * - 1}`,
-                next:next,
-                operation:operation
-              }
-          }
-   default:
-      if (0) return 0
+    case 'AC':
+      return {
+        total: null,
+        next: null,
+        operation: null,
+      };
+    case '+/-':
+      if (total) {
+        return {
+          total: `${total * -1}`,
+          next,
+          operation: null,
+        };
+      }
+      return {
+        total,
+        next: `${next * -1}`,
+        operation: null,
+      };
+
+    case '%':
+      if (total) {
+        return {
+          total: `${total / 100}`,
+          next,
+          operation: null,
+        };
+      }
+      return {
+        total,
+        next: `${next / 100}`,
+        operation: 'X',
+      };
+
+    case '+':
+    case '-':
+    case 'X':
+    case '/':
+      if (total && next) {
+        return {
+          total: operate(total, next, operation),
+          next,
+          operation:btnName,
+        };
+      }
+      return {
+        total,
+      };
+
+    default:
+      if (0) return 0;
 
       return {
-          total,
-          next,
-          operation
-      }
-    }
-}
+        total,
+        next,
+        operation,
+      };
+  }
+};
