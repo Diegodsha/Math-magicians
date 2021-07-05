bb068c9826ad9cca75ad2f5c157e9cdbabf9a2f5
Diego <diegodsha@hotmail.com>
2021-07-05 17:01:38 -0500

    Add exponential formater to operate.js

--- src/Logic/Operate.js
+++ src/Logic/Operate.js
@@ -3,16 +3,22 @@ import Big from 'big.js';
 const operate = (num1, num2, operation) => {
   const big1 = Big(num1);
   const big2 = Big(num2);
+  const checkFormat = (number) => {
+    if (number.length > 12) {
+      return Number(number).toExponential(8);
+    }
+    return number;
+  };
   switch (operation) {
     case '+':
-      return big1.plus(big2).toString();
+      return checkFormat(big1.plus(big2).toString());
     case '-':
-      return big1.minus(big2).toString();
+      return checkFormat(big1.minus(big2).toString());
     case 'X':
-      return big1.times(big2).toString();
+      return checkFormat(big1.times(big2).toString());
     case '÷':
       try {
-        return big1.div(big2).toString();
+        return checkFormat(big1.div(big2).toString());
       } catch {
         return 'Is not possible to divide by 0';
       }
