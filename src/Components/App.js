53597eea8e90996ccfc541e241e290d162b96093
Diego <diegodsha@hotmail.com>
2021-07-05 12:55:27 -0500

    Update app component into class

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// eslint-disable-next-line no-unused-vars
+
 import calculate from '../Logic/Calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
 
-export default App;
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (buttonName) => {
+    // const { calculation } = calculate(this.state, buttonName);
+    this.setState((prevState) => calculate(prevState, buttonName));
+  };
+
+  render() {
+    const { total, next, operation } = this.state;
+    // const result = next ? next && next.toString() : total && total.toString();
+    return (
+      <>
+        <Display result={total || next || '0'} operation={operation} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </>
+    );
+  }
+}
