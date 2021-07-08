15c1091d2241c93c473a048c8576b323e854bcaa
Diego <diegodsha@hotmail.com>
2021-07-07 23:56:03 -0500

    Styled component Display

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Display = ({ result }) => (
-  <div>
-    {result}
-  </div>
-);
+const TotalDisplay = styled.div`
+  background-color: gray;
+  height: 100px;
+  color: white;
+  font-weight: bold;
+  font-size: 3rem;
+  padding: 10px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  border-radius: 5px;
+  margin-bottom: 7px;
+`;
+
+const Display = ({ result }) => <TotalDisplay>{result}</TotalDisplay>;
 
 Display.propTypes = {
   result: PropTypes.string,
