764eb0e6907940e120e51e51166379e693c9ff58
Diego <diegodsha@hotmail.com>
2021-07-08 13:07:23 -0500

    Add home module and styles

---
+++ src/Components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { TextWithShadow } from './Calculator';
+import welcome from '../assets/welcome.jpg';
+
+const StyledHome = styled.div`
+  background-color: #ffffff82;
+  border-radius: 0.5rem;
+  img{
+    width: 500px;
+    border-radius: 0.5rem;
+    margin: 16px 0;
+  }
+`;
+
+const Home = () => (
+  <StyledHome className="container text-center">
+    <TextWithShadow className="text-white display-2">
+      Welcome to Math Magicians!
+    </TextWithShadow>
+    <img className="img-fluid" src={welcome} alt="welcome" />
+    <TextWithShadow className="text-white">
+      We are an enthusiastic team based in France that loves teaching and
+      helping with all topics related to Mathematics. Our goal is that you start
+      loving it as much as we do by providing efficient, fun to use and,
+      beautifull designed web and mobile tools (free to use of course) that will
+      help you throught your learning path and will enhance your experience as a
+      Mathematics lover.
+    </TextWithShadow>
+  </StyledHome>
+);
+
+export default Home;
